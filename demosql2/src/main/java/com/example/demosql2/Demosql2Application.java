b02a78eed1b0ee348ae9859460dd6aa0408bf42c
PoornimaSiddineni <74097675+PoornimaSiddineni@users.noreply.github.com>
2022-10-20 19:07:12 +0530

    Add files via upload

---
+++ demosql2/src/main/java/com/example/demosql2/Demosql2Application.java
@@ -0,0 +1,13 @@
+package com.example.demosql2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demosql2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demosql2Application.class, args);
+    }
+
+}
---
+++ demosql2/src/test/java/com/example/demosql2/Demosql2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demosql2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demosql2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
